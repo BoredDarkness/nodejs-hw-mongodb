13f44c4681e0b3fd7683df9ff9a0eb28377bd967
BoredDarkness <gwinnblaidd@gmail.com>
2025-06-11 01:17:27 +0200

    aaa

---
+++ src/utils/isValidId.js
@@ -0,0 +1,11 @@
+import { Types } from 'mongoose';
+import createError from 'http-errors';
+
+export const isValidId = (req, _, next) => {
+  const { contactId } = req.params;
+  if (!Types.ObjectId.isValid(contactId)) {
+    next(createError(400, 'Invalid contact ID'));
+    return;
+  }
+  next();
+};

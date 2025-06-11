33fbb5eccb7ac19917b94388284b15aa9fc2e552
BoredDarkness <gwinnblaidd@gmail.com>
2025-06-11 22:30:22 +0200

    ,

---
+++ src/routers/auth.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../utils/validateBody.js';
+import {
+  registerController,
+  loginController,
+  refreshController,
+  logoutController,
+} from '../controllers/auth.js';
+import { registerSchema, loginSchema } from '../models/authSchemas.js';
+
+const router = Router();
+
+router.post(
+  '/register',
+  validateBody(registerSchema),
+  ctrlWrapper(registerController),
+);
+
+router.post('/login', validateBody(loginSchema), ctrlWrapper(loginController));
+
+router.post('/refresh', ctrlWrapper(refreshController));
+router.post('/logout', ctrlWrapper(logoutController));
+
+export default router;

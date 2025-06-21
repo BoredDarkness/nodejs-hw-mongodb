562c451d01b0f32a1c3d0b8f99efc7d85215e2cc
BoredDarkness <gwinnblaidd@gmail.com>
2025-06-21 23:42:52 +0200

    Revert "RENDER"
    This reverts commit 686fc1984f96ba0ac509440f703dc9153dac7b0c.

--- src/server.js
+++ src/server.js
@@ -3,30 +3,25 @@ import cors from 'cors';
 import pinoHttp from 'pino-http';
 import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
-
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const swaggerDocPath = path.join(__dirname, '..', 'docs', 'swagger.json');
-const swaggerDocument = JSON.parse(readFileSync(swaggerDocPath, 'utf-8'));
-
 export function setupServer() {
   const app = express();
 
   app.use(cors());
-  app.use(cookieParser());
   app.use(pinoHttp());
   app.use(express.json());
+  app.use(cookieParser());
 
+  const swaggerDocument = JSON.parse(
+    readFileSync(join(process.cwd(), 'docs', 'swagger.json'), 'utf-8'),
+  );
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/auth', authRouter);

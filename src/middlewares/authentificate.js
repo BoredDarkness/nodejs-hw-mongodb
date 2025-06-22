8a3b889db6004952539ca51eba1ae002dc902a6e
BoredDarkness <gwinnblaidd@gmail.com>
2025-06-22 19:48:21 +0200

    authif

--- src/middlewares/authentificate.js
+++ src/middlewares/authentificate.js
@@ -1,28 +1,24 @@
 import createError from 'http-errors';
 import { Session } from '../models/session.js';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
+import { User } from '../models/user.js';
 
-export default async function authenticateRefresh(req, res, next) {
+export default async function authentificate(req, res, next) {
   const header = req.headers.authorization || '';
   const [scheme, token] = header.split(' ');
   if (scheme !== 'Bearer' || !token) {
     return next(createError(401, 'Not authorized'));
   }
 
-  // Перевіряємо, чи є сесія
   const session = await Session.findOne({ accessToken: token });
-  if (!session) {
+  if (!session || session.accessTokenValidUntil < new Date()) {
     return next(createError(401, 'Not authorized'));
   }
 
-  try {
-    jwt.verify(token, process.env.JWT_SECRET);
-  } catch {
+  const user = await User.findById(session.userId).select('-password');
+  if (!user) {
     return next(createError(401, 'Not authorized'));
   }
 
-  req.token = token;
+  req.user = user;
   next();
 }
